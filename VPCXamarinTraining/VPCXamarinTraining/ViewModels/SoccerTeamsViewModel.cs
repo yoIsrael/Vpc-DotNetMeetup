70e6edabfdc3c8a7024616d1d0186417c3d7f571
Luis Fernando Ramírez <luisf350@hotmail.com>
2017-06-08 12:11:33 -0500

    Add some teams to List

--- VPCXamarinTraining/VPCXamarinTraining/ViewModels/SoccerTeamsViewModel.cs
+++ VPCXamarinTraining/VPCXamarinTraining/ViewModels/SoccerTeamsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using VPCXamarinTraining.DTO;
 using VPCXamarinTraining.ViewModels.Base;
 using Xamarin.Forms;
@@ -99,6 +100,80 @@ namespace VPCXamarinTraining.ViewModels
                                 Es regido por la Federación Colombiana de Fútbol y la DIMAYOR a nivel nacional, y por la Confederación Sudamericana de Fútbol (CONMEBOL) y la Federación Internacional de Fútbol Asociación (FIFA) a escala internacional.
                                 A partir del inicio del campeonato colombiano en 1948 logró la mayor cantidad de los títulos locales que se disputaron y conformó un equipo denominado el Ballet Azul, que era considerado de gran importancia a nivel mundial durante la primera parte de la década de los años 1950, cuando consiguió muchos triunfos internacionales, entre los que destacan el Campeonato de las Bodas de Oro del Real Madrid y la Pequeña Copa del Mundo de Clubes de 1953, que le dieron los calificativos del equipo 'Embajador'."
             });
+
+            TeamsList.Add(new SoccerTeamDTO
+            {
+                id = Guid.NewGuid(),
+                Name = "Envigado",
+                Detail = "La Cantera",
+                ImageUrl = "http://as00.epimg.net/img/comunes/fotos/fichas/equipos/large/3841.png",
+                Description = @"El club nace en el municipio de Envigado al sur del Área Metropolitana de Medellín, sin embargo, su debut en el fútbol profesional se dio en el año 1991 con la creación del Torneo de ascenso. En esa primera temporada, Envigado F. C. superó al Alianza Llanos, Atlético Huila y El Cóndor de Bogotá para lograr el cupo en la Primera División. Por aquel entonces, en el equipo naranja militaban jugadores como Hugo Tuberquia, Sergio Guzmán, Samuel Cárdenas, Édgar Carvajal, entre otros, bajo la dirección técnica de Hugo Castaño.
+                                En la campaña 2005-I consiguió por tercera vez clasificarse a los cuadrangulares semifinales de la Primera A.
+                                Posteriormente, el 12 de noviembre de 2006 en desarrollo del Finalización, descendió nuevamente a la Primera B, luego de perder con el Cúcuta Deportivo 1-0, concluyendo una participación de 15 años consecutivos en la primera división.3 Regresando de esta forma al lugar en el cual se inició al fútbol profesional."
+            });
+
+            TeamsList.Add(new SoccerTeamDTO
+            {
+                id = Guid.NewGuid(),
+                Name = "America",
+                Detail = "La Mechita",
+                ImageUrl = "http://as01.epimg.net/img/comunes/fotos/fichas/equipos/large/3952.png",
+                Description = @"La Sociedad Anónima Deportiva América S.A., conocida como América de Cali o simplemente América, es un club de fútbol colombiano de la ciudad de Santiago de Cali; es considerado uno de los clubes más grandes de Colombia y más importantes de Sudamérica.7 Fue fundado el domingo 13 de febrero de 1927. Disputa sus partidos en el Estadio Olímpico Pascual Guerrero, el color que identifica al club desde sus inicios es el rojo escarlata.
+                                Con 13 títulos, es el tercer equipo con más campeonatos oficiales de la Liga Colombiana ganados; también logró ser campeón de la Categoría Primera B en 2016. A nivel internacional tiene cuatro subcampeonatos de la Copa Libertadores de América en los años 1985, 1986, 1987 y 1996. También, logró coronarse como campeón continental en la Copa Merconorte del año 1999; en total suma 15 títulos oficiales entre nacionales e internacionales, además de varios torneos amistosos."
+            });
+
+            TeamsList.Add(new SoccerTeamDTO
+            {
+                id = Guid.NewGuid(),
+                Name = "Deportivo Pasto",
+                Detail = "Los Volcánicos",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/6/6b/Deportivo_Pasto_logo.png",
+                Description = @"El Deportivo Pasto es un club de fútbol de la ciudad de Pasto, Nariño. Fue fundado el 12 de octubre de 1949.1 3 Actualmente juega en la Categoría Primera A del fútbol profesional colombiano y disputa los encuentros como local en el Estadio Departamental Libertad con capacidad para 28.400 espectadores.
+                                Ascendió por primera vez a la máxima categoría en 1998. Históricamente, el Deportivo Pasto fue el primer equipo que llegado del ascenso salió campeón del torneo nacional 2006 y el primero en llegar a una final 2002. En total, el equipo tiene en su haber un título de Categoría Primera A, dos subcampeonatos, 12 presencias en fases finales y dos subcampeonatos de Copa Colombia como máximos logros nacionales y la clasificación a la Copa Sudamericana 2003, la Copa Libertadores 2007 y la Copa Sudamericana 2013 como éxitos internacionales."
+            });
+
+            TeamsList.Add(new SoccerTeamDTO
+            {
+                id = Guid.NewGuid(),
+                Name = "Santa Fe",
+                Detail = "Los Cardenales",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Escudo_de_Independiente_Santa_Fe.svg/1200px-Escudo_de_Independiente_Santa_Fe.svg.png",
+                Description = @"El Club Independiente Santa Fe S. A., conocido como Independiente Santa Fe , o simplemente Santa Fe, es un club de fútbol de la ciudad de Bogotá, capital de Colombia. El club de los Cardenales, o el Expreso Rojo, como es conocido popularmente y considerado uno de los clubes más grandes de Colombia y el mundo durante el año 2016.11 se identifica por ser el primer campeón del fútbol profesional colombiano en la temporada de 1948. 
+                                Es el tercer equipo más exitoso del fútbol colombiano con 16 títulos de primera división en competiciones oficiales, entre nacionales e internacionales. Fue el primer club colombiano en ganar la Copa Sudamericana en 2015 y el primer club colombiano en ganar un trofeo oficial fuera de América, la Copa Suruga Bank en 2016. Ha ganado nueve veces el campeonato de la Categoría Primera A, la más reciente en el Finalización 2016; dos Copas Colombia, en 1989 y 2009; además siendo el más veces campeón de la Superliga de Colombia, en 2013, 2015 y 2017, también posee dos títulos internacionales: la Copa Sudamericana 2015 y la Copa Suruga Bank 2016."
+            });
+
+            TeamsList.Add(new SoccerTeamDTO
+            {
+                id = Guid.NewGuid(),
+                Name = "Junior",
+                Detail = "El Tiburón",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Escudo_del_Junior_de_Barranquilla.svg/140px-Escudo_del_Junior_de_Barranquilla.svg.png",
+                Description = @"El Club Atlético Junior, también conocido como Junior de Barranquilla, Atlético Junior o simplemente Junior, es un club de fútbol de la ciudad de Barranquilla, Colombia. Fue fundado el 7 de agosto de 1924 convirtiéndose en el tercer club más antiguo del país.12 A nivel deportivo se ha consagrado campeón de la Primera división del fútbol profesional colombiano en siete ocasiones (1977, 1980, 1993, 1995, 2004-II, 2010-I, 2011-II).13 Además, ganó la Copa Colombia 2015, que reúne a los equipos pertenecientes a las categorías profesionales del fútbol colombiano.
+                                A nivel internacional, cuenta con (19) participaciones oficiales en torneos internacionales organizados por la Conmebol. Junior es el quinto club del Fútbol Profesional Colombiano con más participaciones internacionales oficiales, siendo el mejor de la Región Caribe del país ubicado en ese rubro.15 Además, ha sido el único en representar en calidad de selección nacional a Colombia en un Campeonato Sudamericano (actual Copa América) obteniendo la Copa Mariscal Sucre destinada a la mejor selección entre los equipos Bolivarianos."
+            });
+
+            TeamsList.Add(new SoccerTeamDTO
+            {
+                id = Guid.NewGuid(),
+                Name = "Cali",
+                Detail = "Azucareros",
+                ImageUrl = "http://as00.epimg.net/img/comunes/fotos/fichas/equipos/large/4201.png",
+                Description = @"El Deportivo Cali es un club deportivo de la ciudad de Santiago de Cali, en el Departamento del Valle del Cauca. Fundado el 23 de noviembre de 1912 como (Cali Football Club) que después de pasar por dos re-estructuraciones desde su fundación por cuestiones económicas, en el año de 1959 se constituye como asociación y es reconocida oficialmente en 1962 lo que en la actualidad es la Asociación Deportivo Cali, hoy en día tiene 104 años. El Deportivo Cali es más conocido como un club de fútbol, aunque compite en otras disciplinas como el baloncesto,14 el tenis, la natación y futsal.
+                                En sus inicios como equipo amateur conquistó los títulos departamentales en los años 1927, 1928, 1929, 1930, 1931, 1934, 1935, y 1936. En su rama de fútbol profesional, ha obtenido los títulos en 1965, 1967, 1969, 1970, 1974, 1996, 1998, 2005 y 2015 de la Categoría Primera A siendo de esta forma el cuarto club más ganador, además de esto, posee el título de la Copa Colombia, trofeo que obtuvo en el 2010 y el título de Superliga de Colombia del año 2014, lo que lo convierte en el quinto club más ganador de la División Mayor del Fútbol Colombiano con 11 títulos oficiales."
+            });
+
+            TeamsList.Add(new SoccerTeamDTO
+            {
+                id = Guid.NewGuid(),
+                Name = "Once Caldas",
+                Detail = "El Blanco Blanco",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Once_Caldas_logo-svg.svg/1200px-Once_Caldas_logo-svg.svg.png",
+                Description = @"El Once Caldas, es un club de fútbol de Colombia, tiene su sede en la ciudad de Manizales, departamento de Caldas, fue refundado el 15 de enero de 1961 sobre la historia del Deportes Caldas, equipo que había jugado los cuatro primeros campeonatos de la Categoría Primera A entre 1948 y 1952. Actualmente juega en la Categoría Primera A de Colombia.
+                                El color que identifica al club es el blanco, el cual utiliza en su uniforme desde 1961. El equipo ejerce de local en el Estadio Palogrande de Manizales, que tiene una capacidad de 28 678 espectadores.
+                                El Once Caldas ha sido campeón cuatro veces del fútbol profesional colombiano, en 1950, en el Apertura de 2003, en el Apertura de 2009 y en el Finalización de 2010. En 2004 se coronó campeón de la Copa Libertadores de América derrotando en la final al que era el vigente campeón Boca Juniors de Argentina convirtiéndose en el segundo equipo colombiano en conseguir este título."
+            });
+
+            TeamsList = new ObservableCollection<SoccerTeamDTO>(TeamsList.OrderBy(x => x.Name));
         }
 
         #endregion
